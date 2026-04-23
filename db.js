c330019a673830bb892551922adb5276f8fc0c12
Junalvort <106506029+Junalvort@users.noreply.github.com>
2026-04-22 23:47:39 -0400

    Remove Firebase initialization from db.js
    Removed Firebase initialization code and comments.

--- db.js
+++ db.js
@@ -5,11 +5,6 @@
 // ── 1. PEGA AQUÍ TU CONFIGURACIÓN DE FIREBASE ────────────────────────────────
 // (la encuentras en tu proyecto Firebase > Configuración > Tu app web)
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAbT9L872CeyMcIuwHtK5UjyA3jJKAF8i0",
   authDomain: "derivmed.firebaseapp.com",
@@ -18,9 +13,6 @@ const firebaseConfig = {
   messagingSenderId: "742083987090",
   appId: "1:742083987090:web:9c9c32ca82a82cd882484b"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 // ─────────────────────────────────────────────────────────────────────────────
 
 // ── 2. DATOS BASE (se cargan a Firestore la primera vez) ──────────────────────
